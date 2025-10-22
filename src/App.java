53551cbb75c8e2c43b815dd935a7235f9a092238
Matias M. Zuccarelli <zmatiasmartin@gmail.com>
2025-10-22 09:48:03 -0300

    agrego data

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola, agrego nuevo para ver como me comitia!");
     }
 }
