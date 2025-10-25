847f4d312447fb1e03c21286e0ae88d0c7fe3748
Matias M. Zuccarelli <zmatiasmartin@gmail.com>
2025-10-25 11:23:58 -0300

    agregue otras opicones de menu que no se ven

--- src/crudpoo/Main.java
+++ src/crudpoo/Main.java
@@ -11,8 +11,8 @@ import gestionUsuarios.Usuario;
 // Prepara listas en memoria y delega en CrudProductos / CrudCategorias.
 // ============================================================================
 public class Main {
-    public static void main(String[] args) { 
-         
+    public static void main(String[] args) {
+
         final ArrayList<Producto> productos = new ArrayList<>();
         final ArrayList<Categoria> categorias = new ArrayList<>();
         categorias.add(new Categoria("Tecnología"));
@@ -24,16 +24,15 @@ public class Main {
         usuarios.add(new Usuario("matt", "123456"));
         usuarios.add(new Usuario("giiss", "qwerty"));
 
-        
         final CrudProductos crudProd = new CrudProductos(productos, categorias);
         final CrudCategorias crudCat = new CrudCategorias(categorias);
         // manejador de usuario
         final CrudUsuario crudUsuarios = new CrudUsuario(usuarios);
-        boolean logiado = false;
+        String logiado = "";
         String linea;
-         int opcion;
+        int opcion;
         do {
-           
+
             System.out.println("\n=== Menú Incial ===");
             System.out.println("1) Ingresar");
             System.out.println("2) Nuevo Usuario");
@@ -46,30 +45,24 @@ public class Main {
                 opcion = -1;
             }
             if (opcion == 1) // ver si puedo poner los if más lindo, no me gusta así, pero funciona
-                logiado = crudUsuarios.ingresar(); 
-            if (logiado)
+                logiado = crudUsuarios.ingresar();
+            if (opcion == 2) // 0
+                crudUsuarios.crear();
+            if (opcion == 3) // este solo lo deberia hacer el usuario una vez loguiado o un administrador
+                crudUsuarios.actualizar();
+            crudUsuarios.crear();
+            if (opcion == 4) // este solo lo deberia hacer el usuario una vez loguiado o un administrador
+                crudUsuarios.actualizarContrasegna(logiado);
+            if (!logiado.equals(""))
+                System.out.println("\n=== Bienvenidio " + logiado + "!  ===");
                 do {
+                    
                     System.out.println("\n=== Menú Principal ===");
                     System.out.println("1) CRUD de Productos");
                     System.out.println("2) CRUD de Categorías");
                     System.out.println("0) Salir");
                     System.out.print("Opción: ");
                     linea = crudProd.scanner.nextLine();
-                    // que es manejo de excepciones?
-                    // - Es una técnica para manejar errores en tiempo de ejecución.
-                    // - Permite que el programa continúe ejecutándose en lugar de fallar
-                    // abruptamente.
-                    // - Mejora la experiencia de usuario al proporcionar mensajes claros de error.
-                    // que pasa si no manejamos la excepción?
-                    // - El programa podría fallar y cerrarse inesperadamente.
-                    // - El usuario no entendería qué salió mal.
-                    // - Podríamos perder datos o estado importante del programa.
-                    // que es NumberFormatException?
-                    // - Es una excepción que ocurre cuando se intenta convertir una cadena a un
-                    // número
-                    // y la cadena no tiene un formato válido.
-                    // - Es una subclase de IllegalArgumentException.
-                    // - Es común al leer entradas del usuario que no son números.
                     try {
                         opcion = Integer.parseInt(linea.trim());
                     } catch (NumberFormatException e) {
@@ -107,7 +100,7 @@ public class Main {
                                 }
                             } while (op != 0);
                         }
-                        case 0 -> System.out.println("¡Hasta luego!");
+                        case 0 -> System.out.println("Gracias, vuelva pronto!");
                         default -> System.out.println("Opción inválida");
                     }
                 } while (opcion != 0);
--- src/gestionUsuarios/CrudUsuario.java
+++ src/gestionUsuarios/CrudUsuario.java
@@ -35,18 +35,41 @@ public class CrudUsuario extends CrudConsola<Usuario> {
         }
     }
 
+    // seria otro tipo de menu donde vea que puede o no administrar usuario
     @Override
     public void actualizar() {
-        int id = leerEntero("Id de la usuario: ");
+        String id = leerTexto("Alias: ");
         for (Usuario c : usuarios) {
-            if (c.getId() == id) {
-                String nuevo = leerTexto("Nuevo nombre: ");
-                c.setNombre(nuevo);
-                System.out.println("Actualizada: " + c);
+            if (c.getAlias() == id) {
+                String nuevo = leerTexto("Nuevo alias: ");
+                // veria de hacer validaciones antes de setear
+                if (chequearNoSeRepita(nuevo)) {
+                    c.setNombre(nuevo);
+                    System.out.println("Actualizada: " + c);
+                } else
+                    System.out.println("Atención! No se actualizó: alias existente: " + c);
                 return;
             }
         }
         System.out.println("No se encontró usuario con id " + id);
+    }
+
+    public void actualizarContrasegna(String alias) {        
+        for (Usuario c : usuarios) {
+            if (c.getAlias() == alias) {
+                String nuevo = leerTexto("Nuevo password: ");
+                // veria de hacer validaciones antes de setear , ver que tenga un punto,
+                // caraceteres especiales
+                if (chequearFormatoValido(nuevo)) {
+                    c.setPassword(nuevo);
+                    System.out.println("Password Actualizado: " + c);
+                } else
+                    System.out.println("Atención! No se actualizó: formato incorrecto: " + c);
+                return;
+            }
+        }
+        // este no deberia pasar si está logiado...
+        System.out.println("No se encontró alias " + alias);
     }
 
     @Override
@@ -56,27 +79,41 @@ public class CrudUsuario extends CrudConsola<Usuario> {
         System.out.println(eliminado ? "usuario eliminada." : "No existía ese id.");
     }
 
-    public boolean ingresar() {
+    // lo habia hecho bool, pero lo cambie para tener el nombre del usuario
+    public String ingresar() {
         String alias;
         String password;
-        boolean logiado = false;
+        String logiado = "";
         System.out.println("\n=== ingreso usuario ===");
         alias = leerTexto("Alias: ");
         password = leerTexto("Contraseña: ");
         // asumo que escribe bien el user y pass, despues haria chequeos, hoy no
 
         for (Usuario c : usuarios) {
-            System.out.println(c.getAlias() == alias);
+           // System.out.println(c.getAlias() == alias);
             if (alias.equals(c.getAlias()) && password.equals(c.getPassword())) {
-                logiado = true;
+                logiado = alias;
                 // return; // no sé porque no anda, me obliga a recorrer todo = deberia andar
                 // poco eficiente
             }
         }
-        if (!logiado)
+        if (alias.equals(""))
             System.out.println("ERROR!--- Alias o Contraseña incorrecta/s");
 
         return logiado;
+    }
+
+    private boolean chequearNoSeRepita(String nuevo) {
+        for (Usuario c : usuarios) {
+            if (nuevo.equals(c.getAlias())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean chequearFormatoValido(String caracteresValidos) {
+        return true;
     }
 
 }
